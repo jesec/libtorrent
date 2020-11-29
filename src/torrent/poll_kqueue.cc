jesec/libtorrent

commit 1c7ee2981cfa421662cbdb74087e438a46e146d0

Replace excessive functional templates with straightforward lambdas

src/torrent/poll_kqueue.cc
@@ -23,7 +23,6 @@
 #include "torrent/poll_kqueue.h"
 #include "torrent/torrent.h"
 #include "torrent/utils/error_number.h"
-#include "torrent/utils/functional.h"
 #include "torrent/utils/log.h"
 #include "torrent/utils/thread_base.h"
 #include "torrent/utils/timer.h"
@@ -318,7 +317,7 @@ PollKQueue::close(Event* event) {
   m_changedEvents =
     std::remove_if(m_changes,
                    m_changes + m_changedEvents,
-                   utils::equal(event, utils::mem_ref(&kevent::udata))) -
+                   [event](kevent& e) { return event == e.udata; }) -
     m_changes;
 }
 
@@ -349,7 +348,7 @@ PollKQueue::closed(Event* event) {
   m_changedEvents =
     std::remove_if(m_changes,
                    m_changes + m_changedEvents,
-                   utils::equal(event, utils::mem_ref(&kevent::udata))) -
+                   [event](kevent& e) { return event == e.udata; }) -
     m_changes;
 }
 

extra/object.cc
@@ -3,8 +3,6 @@
 
 #include <algorithm>
 
-#include "torrent/utils/functional.h"
-
 #include "object.h"
 
 namespace torrent {
@@ -142,10 +140,9 @@ Object::merge_copy(const Object& object, uint32_t maxDepth) {
 
     while (srcItr != srcLast) {
       destItr = std::find_if(
-        destItr,
-        dest.end(),
-        utils::less_equal(srcItr->first,
-                          utils::mem_ref(&map_type::value_type::first)));
+        destItr, dest.end(), [srcItr](const map_type::value_type v) {
+          return srcItr->first <= v.first;
+        });
 
       if (srcItr->first < destItr->first)
         // destItr remains valid and pointing to the next possible

extra/test_queue.cc
@@ -1,7 +1,6 @@
 #include <cstdlib>
 #include <iostream>
 
-#include "torrent/utils/functional.h"
 #include "torrent/utils/priority_queue_default.h"
 
 torrent::utils::priority_queue_default
@@ -51,16 +50,6 @@ main() {
 
       priority_queue_insert(&queue, first, (std::rand() % 50) + 1);
     }
-
-    //     std::vector<torrent::utils::priority_item*> due;
-
-    //     std::copy(torrent::utils::queue_popper(queue,
-    //     torrent::utils::bind2nd(std::mem_fn(&torrent::utils::priority_item::compare),
-    //     20)),
-    // 	      torrent::utils::queue_popper(queue,
-    // torrent::utils::bind2nd(std::mem_fn(&torrent::utils::priority_item::compare),
-    // torrent::utils::timer())), 	      std::back_inserter(due));
-    //     std::for_each(due.begin(), due.end(), std::ptr_fun(&print_item));
 
     while (!queue.empty()) {
       torrent::utils::priority_item* i = queue.top();

include/download/download_main.h
@@ -15,7 +15,6 @@
 #include "torrent/download/group_entry.h"
 #include "torrent/download_info.h"
 #include "torrent/peer/peer_list.h"
-#include "torrent/utils/functional.h"
 
 namespace torrent {
 
@@ -140,17 +139,12 @@ public:
   void setup_delegator();
   void setup_tracker();
 
-  typedef utils::const_mem_fun1<HandshakeManager, uint32_t, DownloadMain*>
-    SlotCountHandshakes;
-  typedef utils::mem_fun1<DownloadWrapper, void, ChunkHandle> SlotHashCheckAdd;
+  typedef std::function<uint32_t(DownloadMain*)> SlotCountHandshakes;
+  typedef std::function<void(ChunkHandle)>       SlotHashCheckAdd;
 
-  typedef utils::mem_fun2<HandshakeManager,
-                          void,
-                          const utils::socket_address&,
-                          DownloadMain*>
-    slot_start_handshake_type;
-  typedef utils::mem_fun1<HandshakeManager, void, DownloadMain*>
-    slot_stop_handshakes_type;
+  typedef std::function<void(const utils::socket_address&, DownloadMain*)>
+                                             slot_start_handshake_type;
+  typedef std::function<void(DownloadMain*)> slot_stop_handshakes_type;
 
   void slot_start_handshake(slot_start_handshake_type s) {
     m_slotStartHandshake = s;

include/protocol/handshake_manager.h
@@ -7,7 +7,6 @@
 
 #include "net/socket_fd.h"
 #include "torrent/connection_manager.h"
-#include "torrent/utils/functional.h"
 #include "torrent/utils/socket_address.h"
 #include "torrent/utils/unordered_vector.h"
 

include/torrent/utils/functional.h
@@ -10,712 +10,6 @@
 namespace torrent {
 namespace utils {
 
-template<typename Type>
-struct reference_fix {
-  typedef Type type;
-};
-
-template<typename Type>
-struct reference_fix<Type&> {
-  typedef Type type;
-};
-
-template<typename Type>
-struct value_t {
-  value_t(Type v)
-    : m_v(v) {}
-
-  Type operator()() const {
-    return m_v;
-  }
-
-  Type m_v;
-};
-
-template<typename Type>
-inline value_t<Type>
-value(Type v) {
-  return value_t<Type>(v);
-}
-
-template<typename Type, typename Ftor>
-struct accumulate_t {
-  accumulate_t(Type t, Ftor f)
-    : result(t)
-    , m_f(f) {}
-
-  template<typename Arg>
-  void operator()(const Arg& a) {
-    result += m_f(a);
-  }
-
-  Type result;
-  Ftor m_f;
-};
-
-template<typename Type, typename Ftor>
-inline accumulate_t<Type, Ftor>
-accumulate(Type t, Ftor f) {
-  return accumulate_t<Type, Ftor>(t, f);
-}
-
-// Operators:
-
-template<typename Type, typename Ftor>
-struct equal_t {
-  typedef bool result_type;
-
-  equal_t(Type t, Ftor f)
-    : m_t(t)
-    , m_f(f) {}
-
-  template<typename Arg>
-  bool operator()(Arg& a) {
-    return m_t == m_f(a);
-  }
-
-  Type m_t;
-  Ftor m_f;
-};
-
-template<typename Type, typename Ftor>
-inline equal_t<Type, Ftor>
-equal(Type t, Ftor f) {
-  return equal_t<Type, Ftor>(t, f);
-}
-
-template<typename Type, typename Ftor>
-struct equal_ptr_t {
-  typedef bool result_type;
-
-  equal_ptr_t(Type* t, Ftor f)
-    : m_t(t)
-    , m_f(f) {}
-
-  template<typename Arg>
-  bool operator()(const Arg& a) {
-    return *m_t == *m_f(a);
-  }
-
-  Type* m_t;
-  Ftor  m_f;
-};
-
-template<typename Type, typename Ftor>
-inline equal_ptr_t<Type, Ftor>
-equal_ptr(Type* t, Ftor f) {
-  return equal_ptr_t<Type, Ftor>(t, f);
-}
-
-template<typename Type, typename Ftor>
-struct not_equal_t {
-  typedef bool result_type;
-
-  not_equal_t(Type t, Ftor f)
-    : m_t(t)
-    , m_f(f) {}
-
-  template<typename Arg>
-  bool operator()(Arg& a) {
-    return m_t != m_f(a);
-  }
-
-  Type m_t;
-  Ftor m_f;
-};
-
-template<typename Type, typename Ftor>
-inline not_equal_t<Type, Ftor>
-not_equal(Type t, Ftor f) {
-  return not_equal_t<Type, Ftor>(t, f);
-}
-
-template<typename Type, typename Ftor>
-struct less_t {
-  typedef bool result_type;
-
-  less_t(Type t, Ftor f)
-    : m_t(t)
-    , m_f(f) {}
-
-  template<typename Arg>
-  bool operator()(Arg& a) {
-    return m_t < m_f(a);
-  }
-
-  Type m_t;
-  Ftor m_f;
-};
-
-template<typename Type, typename Ftor>
-inline less_t<Type, Ftor>
-less(Type t, Ftor f) {
-  return less_t<Type, Ftor>(t, f);
-}
-
-template<typename FtorA, typename FtorB>
-struct less2_t
-  : public std::binary_function<typename FtorA::argument_type,
-                                typename FtorB::argument_type,
-                                bool> {
-  less2_t(FtorA f_a, FtorB f_b)
-    : m_f_a(f_a)
-    , m_f_b(f_b) {}
-
-  bool operator()(typename FtorA::argument_type a,
-                  typename FtorB::argument_type b) {
-    return m_f_a(a) < m_f_b(b);
-  }
-
-  FtorA m_f_a;
-  FtorB m_f_b;
-};
-
-template<typename FtorA, typename FtorB>
-inline less2_t<FtorA, FtorB>
-less2(FtorA f_a, FtorB f_b) {
-  return less2_t<FtorA, FtorB>(f_a, f_b);
-}
-
-template<typename Type, typename Ftor>
-struct _greater {
-  typedef bool result_type;
-
-  _greater(Type t, Ftor f)
-    : m_t(t)
-    , m_f(f) {}
-
-  template<typename Arg>
-  bool operator()(Arg& a) {
-    return m_t > m_f(a);
-  }
-
-  Type m_t;
-  Ftor m_f;
-};
-
-template<typename Type, typename Ftor>
-inline _greater<Type, Ftor>
-greater(Type t, Ftor f) {
-  return _greater<Type, Ftor>(t, f);
-}
-
-template<typename FtorA, typename FtorB>
-struct greater2_t
-  : public std::binary_function<typename FtorA::argument_type,
-                                typename FtorB::argument_type,
-                                bool> {
-  greater2_t(FtorA f_a, FtorB f_b)
-    : m_f_a(f_a)
-    , m_f_b(f_b) {}
-
-  bool operator()(typename FtorA::argument_type a,
-                  typename FtorB::argument_type b) {
-    return m_f_a(a) > m_f_b(b);
-  }
-
-  FtorA m_f_a;
-  FtorB m_f_b;
-};
-
-template<typename FtorA, typename FtorB>
-inline greater2_t<FtorA, FtorB>
-greater2(FtorA f_a, FtorB f_b) {
-  return greater2_t<FtorA, FtorB>(f_a, f_b);
-}
-
-template<typename Type, typename Ftor>
-struct less_equal_t {
-  typedef bool result_type;
-
-  less_equal_t(Type t, Ftor f)
-    : m_t(t)
-    , m_f(f) {}
-
-  template<typename Arg>
-  bool operator()(Arg& a) {
-    return m_t <= m_f(a);
-  }
-
-  Type m_t;
-  Ftor m_f;
-};
-
-template<typename Type, typename Ftor>
-inline less_equal_t<Type, Ftor>
-less_equal(Type t, Ftor f) {
-  return less_equal_t<Type, Ftor>(t, f);
-}
-
-template<typename Type, typename Ftor>
-struct greater_equal_t {
-  typedef bool result_type;
-
-  greater_equal_t(Type t, Ftor f)
-    : m_t(t)
-    , m_f(f) {}
-
-  template<typename Arg>
-  bool operator()(Arg& a) {
-    return m_t >= m_f(a);
-  }
-
-  Type m_t;
-  Ftor m_f;
-};
-
-template<typename Type, typename Ftor>
-inline greater_equal_t<Type, Ftor>
-greater_equal(Type t, Ftor f) {
-  return greater_equal_t<Type, Ftor>(t, f);
-}
-
-template<typename Tp>
-struct invert : public std::unary_function<Tp, Tp> {
-  Tp operator()(const Tp& x) const {
-    return ~x;
-  }
-};
-
-template<typename Src, typename Dest>
-struct on_t
-  : public std::unary_function<typename Src::argument_type,
-                               typename Dest::result_type> {
-  typedef typename Dest::result_type result_type;
-
-  on_t(Src s, Dest d)
-    : m_dest(d)
-    , m_src(s) {}
-
-  result_type operator()(
-    typename reference_fix<typename Src::argument_type>::type arg) {
-    return m_dest(m_src(arg));
-  }
-
-  Dest m_dest;
-  Src  m_src;
-};
-
-template<typename Src, typename Dest>
-inline on_t<Src, Dest>
-on(Src s, Dest d) {
-  return on_t<Src, Dest>(s, d);
-}
-
-template<typename Src, typename Dest>
-struct on2_t
-  : public std::binary_function<typename Src::argument_type,
-                                typename Dest::second_argument_type,
-                                typename Dest::result_type> {
-  typedef typename Dest::result_type result_type;
-
-  on2_t(Src s, Dest d)
-    : m_dest(d)
-    , m_src(s) {}
-
-  result_type operator()(
-    typename reference_fix<typename Src::argument_type>::type         first,
-    typename reference_fix<typename Dest::second_argument_type>::type second) {
-    return m_dest(m_src(first), second);
-  }
-
-  Dest m_dest;
-  Src  m_src;
-};
-
-template<typename Src, typename Dest>
-inline on2_t<Src, Dest>
-on2(Src s, Dest d) {
-  return on2_t<Src, Dest>(s, d);
-}
-
-// Creates a functor for accessing a member.
-template<typename Class, typename Member>
-struct mem_ptr_t : public std::unary_function<Class*, Member&> {
-  mem_ptr_t(Member Class::*m)
-    : m_member(m) {}
-
-  Member& operator()(Class* c) {
-    return c->*m_member;
-  }
-
-  const Member& operator()(const Class* c) {
-    return c->*m_member;
-  }
-
-  Member Class::*m_member;
-};
-
-template<typename Class, typename Member>
-inline mem_ptr_t<Class, Member>
-mem_ptr(Member Class::*m) {
-  return mem_ptr_t<Class, Member>(m);
-}
-
-template<typename Class, typename Member>
-struct mem_ref_t : public std::unary_function<Class&, Member&> {
-  mem_ref_t(Member Class::*m)
-    : m_member(m) {}
-
-  Member& operator()(Class& c) {
-    return c.*m_member;
-  }
-
-  Member Class::*m_member;
-};
-
-template<typename Class, typename Member>
-struct const_mem_ref_t
-  : public std::unary_function<const Class&, const Member&> {
-  const_mem_ref_t(const Member Class::*m)
-    : m_member(m) {}
-
-  const Member& operator()(const Class& c) {
-    return c.*m_member;
-  }
-
-  const Member Class::*m_member;
-};
-
-template<typename Class, typename Member>
-inline mem_ref_t<Class, Member>
-mem_ref(Member Class::*m) {
-  return mem_ref_t<Class, Member>(m);
-}
-
-template<typename Class, typename Member>
-inline const_mem_ref_t<Class, Member>
-const_mem_ref(const Member Class::*m) {
-  return const_mem_ref_t<Class, Member>(m);
-}
-
-template<typename Cond, typename Then>
-struct if_then_t {
-  if_then_t(Cond c, Then t)
-    : m_cond(c)
-    , m_then(t) {}
-
-  template<typename Arg>
-  void operator()(Arg& a) {
-    if (m_cond(a))
-      m_then(a);
-  }
-
-  Cond m_cond;
-  Then m_then;
-};
-
-template<typename Cond, typename Then>
-inline if_then_t<Cond, Then>
-if_then(Cond c, Then t) {
-  return if_then_t<Cond, Then>(c, t);
-}
-
-template<typename T>
-struct call_delete : public std::unary_function<T*, void> {
-  void operator()(T* t) {
-    delete t;
-  }
-};
-
-template<typename T>
-inline void
-call_delete_func(T* t) {
-  delete t;
-}
-
-template<typename Operation>
-class bind1st_t
-  : public std::unary_function<typename Operation::second_argument_type,
-                               typename Operation::result_type> {
-public:
-  typedef typename reference_fix<typename Operation::first_argument_type>::type
-    value_type;
-  typedef typename reference_fix<typename Operation::second_argument_type>::type
-    argument_type;
-
-  bind1st_t(const Operation& op, const value_type v)
-    : m_op(op)
-    , m_value(v) {}
-
-  typename Operation::result_type operator()(const argument_type arg) {
-    return m_op(m_value, arg);
-  }
-
-protected:
-  Operation  m_op;
-  value_type m_value;
-};
-
-template<typename Operation, typename Type>
-inline bind1st_t<Operation>
-bind1st(const Operation& op, const Type& val) {
-  return bind1st_t<Operation>(op, val);
-}
-
-template<typename Operation>
-class bind2nd_t
-  : public std::unary_function<typename Operation::first_argument_type,
-                               typename Operation::result_type> {
-public:
-  typedef typename reference_fix<typename Operation::first_argument_type>::type
-    argument_type;
-  typedef typename reference_fix<typename Operation::second_argument_type>::type
-    value_type;
-
-  bind2nd_t(const Operation& op, const value_type v)
-    : m_op(op)
-    , m_value(v) {}
-
-  typename Operation::result_type operator()(argument_type arg) {
-    return m_op(arg, m_value);
-  }
-
-protected:
-  Operation  m_op;
-  value_type m_value;
-};
-
-template<typename Operation, typename Type>
-inline bind2nd_t<Operation>
-bind2nd(const Operation& op, const Type& val) {
-  return bind2nd_t<Operation>(op, val);
-}
-
-// Lightweight callback function including pointer to object. Should
-// be replaced by TR1 stuff later. Requires an object to bind, instead
-// of using a seperate functor for that.
-
-template<typename Ret>
-class ptr_fun0 {
-public:
-  typedef Ret result_type;
-  typedef Ret (*Function)();
-
-  ptr_fun0() {}
-  ptr_fun0(Function f)
-    : m_function(f) {}
-
-  bool is_valid() const {
-    return m_function;
-  }
-
-  Ret operator()() {
-    return m_function();
-  }
-
-private:
-  Function m_function;
-};
-
-template<typename Object, typename Ret>
-class mem_fun0 {
-public:
-  typedef Ret result_type;
-  typedef Ret (Object::*Function)();
-
-  mem_fun0()
-    : m_object(NULL) {}
-  mem_fun0(Object* o, Function f)
-    : m_object(o)
-    , m_function(f) {}
-
-  bool is_valid() const {
-    return m_object;
-  }
-
-  Ret operator()() {
-    return (m_object->*m_function)();
-  }
-
-private:
-  Object*  m_object;
-  Function m_function;
-};
-
-template<typename Object, typename Ret>
-class const_mem_fun0 {
-public:
-  typedef Ret result_type;
-  typedef Ret (Object::*Function)() const;
-
-  const_mem_fun0()
-    : m_object(NULL) {}
-  const_mem_fun0(const Object* o, Function f)
-    : m_object(o)
-    , m_function(f) {}
-
-  bool is_valid() const {
-    return m_object;
-  }
-
-  Ret operator()() const {
-    return (m_object->*m_function)();
-  }
-
-private:
-  const Object* m_object;
-  Function      m_function;
-};
-
-template<typename Object, typename Ret, typename Arg1>
-class mem_fun1 {
-public:
-  typedef Ret result_type;
-  typedef Ret (Object::*Function)(Arg1);
-
-  mem_fun1()
-    : m_object(NULL) {}
-  mem_fun1(Object* o, Function f)
-    : m_object(o)
-    , m_function(f) {}
-
-  bool is_valid() const {
-    return m_object;
-  }
-
-  Ret operator()(Arg1 a1) {
-    return (m_object->*m_function)(a1);
-  }
-
-private:
-  Object*  m_object;
-  Function m_function;
-};
-
-template<typename Object, typename Ret, typename Arg1>
-class const_mem_fun1 {
-public:
-  typedef Ret result_type;
-  typedef Ret (Object::*Function)(Arg1) const;
-
-  const_mem_fun1()
-    : m_object(NULL) {}
-  const_mem_fun1(const Object* o, Function f)
-    : m_object(o)
-    , m_function(f) {}
-
-  bool is_valid() const {
-    return m_object;
-  }
-
-  Ret operator()(Arg1 a1) const {
-    return (m_object->*m_function)(a1);
-  }
-
-private:
-  const Object* m_object;
-  Function      m_function;
-};
-
-template<typename Object, typename Ret, typename Arg1, typename Arg2>
-class mem_fun2 : public std::binary_function<Arg1, Arg2, Ret> {
-public:
-  typedef Ret result_type;
-  typedef Ret (Object::*Function)(Arg1, Arg2);
-  typedef Object object_type;
-
-  mem_fun2()
-    : m_object(NULL) {}
-  mem_fun2(Object* o, Function f)
-    : m_object(o)
-    , m_function(f) {}
-
-  bool is_valid() const {
-    return m_object;
-  }
-
-  object_type* object() {
-    return m_object;
-  }
-  const object_type* object() const {
-    return m_object;
-  }
-
-  Ret operator()(Arg1 a1, Arg2 a2) {
-    return (m_object->*m_function)(a1, a2);
-  }
-
-private:
-  Object*  m_object;
-  Function m_function;
-};
-
-template<typename Object,
-         typename Ret,
-         typename Arg1,
-         typename Arg2,
-         typename Arg3>
-class mem_fun3 {
-public:
-  typedef Ret result_type;
-  typedef Ret (Object::*Function)(Arg1, Arg2, Arg3);
-
-  mem_fun3()
-    : m_object(NULL) {}
-  mem_fun3(Object* o, Function f)
-    : m_object(o)
-    , m_function(f) {}
-
-  bool is_valid() const {
-    return m_object;
-  }
-
-  Ret operator()(Arg1 a1, Arg2 a2, Arg3 a3) {
-    return (m_object->*m_function)(a1, a2, a3);
-  }
-
-private:
-  Object*  m_object;
-  Function m_function;
-};
-
-template<typename Ret>
-inline ptr_fun0<Ret>
-ptr_fun(Ret (*f)()) {
-  return ptr_fun0<Ret>(f);
-}
-
-template<typename Object, typename Ret>
-inline mem_fun0<Object, Ret>
-make_mem_fun(Object* o, Ret (Object::*f)()) {
-  return mem_fun0<Object, Ret>(o, f);
-}
-
-template<typename Object, typename Ret>
-inline const_mem_fun0<Object, Ret>
-make_mem_fun(const Object* o, Ret (Object::*f)() const) {
-  return const_mem_fun0<Object, Ret>(o, f);
-}
-
-template<typename Object, typename Ret, typename Arg1>
-inline mem_fun1<Object, Ret, Arg1>
-make_mem_fun(Object* o, Ret (Object::*f)(Arg1)) {
-  return mem_fun1<Object, Ret, Arg1>(o, f);
-}
-
-template<typename Object, typename Ret, typename Arg1>
-inline const_mem_fun1<Object, Ret, Arg1>
-make_mem_fun(const Object* o, Ret (Object::*f)(Arg1) const) {
-  return const_mem_fun1<Object, Ret, Arg1>(o, f);
-}
-
-template<typename Object, typename Ret, typename Arg1, typename Arg2>
-inline mem_fun2<Object, Ret, Arg1, Arg2>
-make_mem_fun(Object* o, Ret (Object::*f)(Arg1, Arg2)) {
-  return mem_fun2<Object, Ret, Arg1, Arg2>(o, f);
-}
-
-template<typename Object,
-         typename Ret,
-         typename Arg1,
-         typename Arg2,
-         typename Arg3>
-inline mem_fun3<Object, Ret, Arg1, Arg2, Arg3>
-make_mem_fun(Object* o, Ret (Object::*f)(Arg1, Arg2, Arg3)) {
-  return mem_fun3<Object, Ret, Arg1, Arg2, Arg3>(o, f);
-}
-
 template<typename Container>
 inline void
 slot_list_call(const Container& slot_list) {

include/torrent/utils/ranges.h
@@ -7,8 +7,6 @@
 #include <algorithm>
 #include <vector>
 
-#include <torrent/utils/functional.h>
-
 namespace torrent {
 
 template<typename RangesType>
@@ -65,9 +63,7 @@ ranges<RangesType>::insert(value_type r) {
     return;
 
   iterator first = std::find_if(
-    begin(),
-    end(),
-    utils::less_equal(r.first, utils::const_mem_ref(&value_type::second)));
+    begin(), end(), [r](const value_type v) { return r.first <= v.second; });
 
   if (first == end() || r.second < first->first) {
     // The new range is before the first, after the last or between
@@ -78,10 +74,9 @@ ranges<RangesType>::insert(value_type r) {
     first->first  = std::min(r.first, first->first);
     first->second = std::max(r.second, first->second);
 
-    iterator last = std::find_if(
-      first,
-      end(),
-      utils::less(first->second, utils::const_mem_ref(&value_type::second)));
+    iterator last = std::find_if(first, end(), [first](const value_type v) {
+      return first->second < v.second;
+    });
 
     if (last != end() && first->second >= last->first)
       first->second = (last++)->second;
@@ -97,13 +92,9 @@ ranges<RangesType>::erase(value_type r) {
     return;
 
   iterator first = std::find_if(
-    begin(),
-    end(),
-    utils::less(r.first, utils::const_mem_ref(&value_type::second)));
+    begin(), end(), [r](const value_type v) { return r.first < v.second; });
   iterator last = std::find_if(
-    first,
-    end(),
-    utils::less(r.second, utils::const_mem_ref(&value_type::second)));
+    first, end(), [r](const value_type v) { return r.second < v.second; });
 
   if (first == end())
     return;
@@ -135,18 +126,14 @@ template<typename RangesType>
 inline typename ranges<RangesType>::iterator
 ranges<RangesType>::find(bound_type index) {
   return std::find_if(
-    begin(),
-    end(),
-    utils::less(index, utils::const_mem_ref(&value_type::second)));
+    begin(), end(), [index](const value_type v) { return index < v.second; });
 }
 
 template<typename RangesType>
 inline typename ranges<RangesType>::const_iterator
 ranges<RangesType>::find(bound_type index) const {
   return std::find_if(
-    begin(),
-    end(),
-    utils::less(index, utils::const_mem_ref(&value_type::second)));
+    begin(), end(), [index](const value_type v) { return index < v.second; });
 }
 
 // Use find with no closest match.

src/data/chunk_list.cc
@@ -7,7 +7,6 @@
 #include "torrent/data/download_data.h"
 #include "torrent/exceptions.h"
 #include "torrent/utils/error_number.h"
-#include "torrent/utils/functional.h"
 #include "torrent/utils/log.h"
 #include "utils/instrumentation.h"
 
@@ -293,9 +292,9 @@ ChunkList::sync_chunks(int flags) {
     split = m_queue.begin();
   else
     split = std::stable_partition(
-      m_queue.begin(),
-      m_queue.end(),
-      utils::not_equal(1, std::mem_fn(&ChunkListNode::writable)));
+      m_queue.begin(), m_queue.end(), [](ChunkListNode* n) {
+        return 1 != n->writable();
+      });
 
   // Allow a flag that does more culling, so that we only get large
   // continous sections.

commit 82a26c13aede1a0a1c4939032fa2a91533a7b9c7

poll_kqueue: use 'struct' tag to refer to type 'kevent'
Fixes: 1c7ee2981c

src/torrent/poll_kqueue.cc
@@ -317,7 +317,7 @@ PollKQueue::close(Event* event) {
   m_changedEvents =
     std::remove_if(m_changes,
                    m_changes + m_changedEvents,
-                   [event](kevent& e) { return event == e.udata; }) -
+                   [event](struct kevent& e) { return event == e.udata; }) -
     m_changes;
 }
 
@@ -348,7 +348,7 @@ PollKQueue::closed(Event* event) {
   m_changedEvents =
     std::remove_if(m_changes,
                    m_changes + m_changedEvents,
-                   [event](kevent& e) { return event == e.udata; }) -
+                   [event](struct kevent& e) { return event == e.udata; }) -
     m_changes;
 }
 