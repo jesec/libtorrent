2d7fb9f2df51608a7dcff5ee574eb9b0012a7dad
Jesse Chan <jc@linux.com>
2021-02-26 20:05:05 +0800

    net/types: fix allocation/deallocation mismatch

--- include/torrent/net/types.h
+++ include/torrent/net/types.h
@@ -2,12 +2,61 @@
 #define LIBTORRENT_NET_TYPES_H
 
 #include <memory>
+#include <netinet/in.h>
 #include <sys/socket.h>
-#include <tuple>
+#include <sys/un.h>
 
-struct sockaddr_in;
-struct sockaddr_in6;
-struct sockaddr_un;
+namespace std {
+template<>
+struct default_delete<sockaddr> {
+  default_delete() = default;
+  template<class U>
+  constexpr default_delete(default_delete<U>) noexcept {}
+  void operator()(sockaddr* ptr) const noexcept {
+    if (ptr != nullptr) {
+      switch (ptr->sa_family) {
+        case AF_INET:
+          delete reinterpret_cast<sockaddr_in*>(ptr);
+          return;
+        case AF_INET6:
+          delete reinterpret_cast<sockaddr_in6*>(ptr);
+          return;
+        case AF_UNIX:
+          delete reinterpret_cast<sockaddr_un*>(ptr);
+          return;
+        default:
+          delete ptr;
+          return;
+      }
+    }
+  }
+};
+
+template<>
+struct default_delete<const sockaddr> {
+  default_delete() = default;
+  template<class U>
+  constexpr default_delete(default_delete<U>) noexcept {}
+  void operator()(const sockaddr* ptr) const noexcept {
+    if (ptr != nullptr) {
+      switch (ptr->sa_family) {
+        case AF_INET:
+          delete reinterpret_cast<sockaddr_in*>(const_cast<sockaddr*>(ptr));
+          return;
+        case AF_INET6:
+          delete reinterpret_cast<sockaddr_in6*>(const_cast<sockaddr*>(ptr));
+          return;
+        case AF_UNIX:
+          delete reinterpret_cast<sockaddr_un*>(const_cast<sockaddr*>(ptr));
+          return;
+        default:
+          delete const_cast<sockaddr*>(ptr);
+          return;
+      }
+    }
+  }
+};
+}
 
 namespace torrent {
 
@@ -21,7 +70,7 @@ using c_sin_unique_ptr  = std::unique_ptr<const sockaddr_in>;
 using c_sin6_unique_ptr = std::unique_ptr<const sockaddr_in6>;
 using c_sun_unique_ptr  = std::unique_ptr<const sockaddr_un>;
 
-using fd_sap_tuple = std::tuple<int, std::unique_ptr<sockaddr>>;
+using fd_sap_tuple = std::tuple<int, sa_unique_ptr>;
 
 struct listen_result_type {
   int           fd;
